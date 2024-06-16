e4a1ae725ba10e480c737bea4fb4db85929a4443
mpooja9 <poojamishra.nk@gmail.com>
2024-06-16 12:52:12 +0530

    ui update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import MainNav from "./MainNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6">
+    <div className="border-b-2 border-b-green-500 py-6">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-bold tracking-tight text-orange-500"
+          className="text-3xl font-bold tracking-tight text-green-500"
         >
-          MernEats.com
+          NomNomNow
         </Link>
         <div className="md:hidden">
           <MobileNav />
